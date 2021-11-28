fd11c11ba430c22cfff495b0a9392957fd29308d
Alen Jo <alenjo874@gmail.com>
2021-11-28 12:22:46 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+let removeMain = document.getElementById("main");
+removeMain.remove();
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "<p> Alen is the champion </p>";
+
+document.body.append(newHeader);
